e303f1b1137d42ff0a566bea35d8207fb076f4b5
chaoscollective <admin@chaoscollective.org>
2013-10-04 15:21:37 +0000

    update from chaos space editor.

--- jsworkers/lame/encoder.js
+++ jsworkers/lame/encoder.js
@@ -15,7 +15,14 @@ self.onmessage = function(e) {
     Lame.set_num_channels(mp3codec, chans);
     Lame.set_in_samplerate(mp3codec, e.data.config.in_samplerate || 44100);
     Lame.set_out_samplerate(mp3codec, e.data.config.out_samplerate || 44100);
-    Lame.set_bitrate(mp3codec, e.data.config.bitrate || 128);
+    if(!e.data.config.vbr){
+      Lame.set_bitrate(mp3codec, e.data.config.bitrate || 128);
+    }else{
+      Lame.set_VBR(mp3codec, e.data.config.vbr || 1); 
+      Lame.set_VBR_min_bitrate_kbps(mp3codec, e.data.config.vbr_min || 16);
+      Lame.set_VBR_max_bitrate_kbps(mp3codec, e.data.config.vbr_max || 128);
+      Lame.set_VBR_q(mp3codec, e.data.config.vbr_q || 4);
+    }
     Lame.init_params(mp3codec);
     break;
   case 'encode':
