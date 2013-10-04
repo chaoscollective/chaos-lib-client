dc7b22ff306a675305b057aaeffd0c5a3a46f0ed
chaoscollective <admin@chaoscollective.org>
2013-10-04 13:39:31 +0000

    update from chaos space editor.

--- jsworkers/lame/encoder.js
+++ jsworkers/lame/encoder.js
@@ -10,8 +10,9 @@ self.onmessage = function(e) {
   case 'init':
     e.data.config = e.data.config||{};
     mp3codec = Lame.init();
-    Lame.set_mode(mp3codec, e.data.config.mode || Lame.JOINT_STEREO);
-    Lame.set_num_channels(mp3codec, e.data.config.channels || 2);
+    var chans = e.data.config.channels || 2; 
+    Lame.set_mode(mp3codec, e.data.config.mode || (chans===1)?Lame.MONO:Lame.JOINT_STEREO);
+    Lame.set_num_channels(mp3codec, chans);
     Lame.set_in_samplerate(mp3codec, e.data.config.in_samplerate || 44100);
     Lame.set_out_samplerate(mp3codec, e.data.config.out_samplerate || 44100);
     Lame.set_bitrate(mp3codec, e.data.config.bitrate || 128);
